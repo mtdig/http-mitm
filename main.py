d0546ef1d615b4a37d23b592c7b217f421b14c67
jeroen <jeroen.vanrenterghem@student.hogent.be>
2026-03-28 10:16:23 +0100

    feat: add code

---
+++ main.py
@@ -0,0 +1,15 @@
+import uvicorn
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+
+@app.post("/login")
+async def login(request: Request):
+    body = await request.json()
+    print(f"\n>>> {body}")
+    return {"status": "ok"}
+
+
+def run():
+    uvicorn.run(app, host="0.0.0.0", port=8080)
